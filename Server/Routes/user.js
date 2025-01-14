b8adde8ae92797609acbddc44d844af8d0472fc5
bhanusrib <bhimavarambhanusri.aiml2023@citchennainet>
2025-01-14 22:23:24 +0530

    Fix case sensitivity in Controller imports

--- Server/Routes/user.js
+++ Server/Routes/user.js
@@ -1,13 +1,13 @@
 import express from "express"
 import { login } from "../Controllers/Auth.js"
 import { updatechaneldata,getallchanels } from "../Controllers/channel.js";
-import { createChatRoom } from "../Controllers/chatRoom.js"; // Import the new function
+import { createChatRoom } from "../Controllers/ChatRoom.js"; // Fixed case sensitivity
 
 const routes=express.Router();
 
 routes.post('/login',login)
 routes.patch('/update/:id',updatechaneldata)
 routes.get('/getallchannel',getallchanels)
-routes.post('/create-chat-room', createChatRoom) // Add the new route
+routes.post('/create-chat-room', createChatRoom)
 
 export default routes;
